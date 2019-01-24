4dd451a644bb0c7c5ed7c1c96e0b3c2ba8979018
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-24 23:49:45 +0200

    index

--- src/main/java/org/revo/index/IndexApplication.java
+++ src/main/java/org/revo/index/IndexApplication.java
@@ -1,5 +1,6 @@
 package org.revo.index;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -7,7 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
+
+import java.util.stream.Collectors;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -24,8 +26,8 @@ public class IndexApplication {
 
 
     @Bean
-    public RouterFunction<ServerResponse> function(DiscoveryClient discoveryClient) {
-        return route(GET("/"), serverRequest -> ok().body(fromIterable(discoveryClient.getServices()), String.class));
+    public RouterFunction<ServerResponse> function(@Value("${message:default}") String message, DiscoveryClient discoveryClient) {
+        return route(GET("/"), serverRequest -> ok().body(fromIterable(discoveryClient.getServices().stream().map(it -> message + " " + it).collect(Collectors.toList())), String.class));
     }
 }
 
