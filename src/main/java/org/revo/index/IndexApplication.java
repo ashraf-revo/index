3d2ab7ab664ea4a21ee51ff82d64260afeedb0be
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-24 20:54:58 +0200

    index

--- src/main/java/org/revo/index/IndexApplication.java
+++ src/main/java/org/revo/index/IndexApplication.java
@@ -1,18 +1,21 @@
 package org.revo.index;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
-import static reactor.core.publisher.Mono.just;
+import static reactor.core.publisher.Flux.fromIterable;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IndexApplication {
 
     public static void main(String[] args) {
@@ -21,8 +24,8 @@ public class IndexApplication {
 
 
     @Bean
-    public RouterFunction<ServerResponse> function(@Value("${message:default}") String message) {
-        return route(GET("/"), serverRequest -> ok().body(just(message), String.class));
+    public RouterFunction<ServerResponse> function(DiscoveryClient discoveryClient) {
+        return route(GET("/"), serverRequest -> ok().body(fromIterable(discoveryClient.getServices()), String.class));
     }
 }
 
